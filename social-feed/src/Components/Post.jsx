0a6312eae4f3c8b1f2dc8f40152600115aaf1587
Victoria Probst <victoria.probst13@gmail.com>
2022-06-16 12:20:23 -0400

    Created Visual Representation of Like and Dislike of Posts

--- social-feed/src/Components/Post.jsx
+++ social-feed/src/Components/Post.jsx
@@ -1,29 +1,30 @@
-import React, { useState} from 'react';
+import React from 'react';
+import CustomButton from './CustomBotton/CustomButton';
 
 const Post = ({post}) => {
 
-    const [like, setLike]= useState('');
-    const [dislike, setDislike] = useState('');
-
-    function likeButton(event){                                     //With LIKE BUTTON functionality, it is hardcoded as False Boolean
-        event.preventDefault();                                     // I need to flip that Boolean to True and print TRUE to the console.  
-        let like = false;
-        if (!like === post.isLiked) {
-            return true;
-        }
-        console.log(dislike);
-       
-        
-    }
-
-   function disLikeButton(event){
-    event.preventDefault();
-       let dislike = false;
-    if(!dislike === post.isDisliked){
-            return true;
-        }
-        console.log(like);
-    }
+    //const [like, setLike]= useState(true);
+    //const [dislike, setDislike] = useState('');
+//
+    //function likeButton(event){                                     //With LIKE BUTTON functionality, it is hardcoded as False Boolean
+    //    event.preventDefault();                                     // I need to flip that Boolean to True and print TRUE to the console.  
+    //    let like = false;                                           //update the array to show the updated boolean
+    //    if (!like === post.isLiked) {
+    //        return true;
+    //    }
+    //    console.log(dislike);
+    //   
+    //    
+    //}
+//
+   //function disLikeButton(event){
+    //event.preventDefault();
+    //   let dislike = false;
+    //if(!dislike === post.isDisliked){
+    //        return true;
+    //    }
+    //    console.log(like);
+    //}
 
     //onClick = !disLike
 
@@ -33,8 +34,8 @@ const Post = ({post}) => {
         <div>
             <h3> {post.name}</h3>
             <p> {post.message}</p>
-            <button onClick={disLikeButton} onChange={(event)=>setLike(event.target.value) } >Like</button>
-            <button onClick={likeButton} onChange={(event)=> setDislike(event.target.value)}>Dislike</button>
+            <CustomButton message= "Like"/>
+            <CustomButton message="Dislike"/>
         </div>
      );
 }
